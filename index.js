d918e2c966739659f4e68f17b1e141d3bb0e32df
NeelimaMnr <113683683+NeelimaMnr@users.noreply.github.com>
2023-11-22 09:39:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+
+
+document.getElementById('sideBarBtn').addEventListener('click', function () {
+    // Get a collection of elements with class 'item'
+    const items = document.getElementsByClassName('item');
+
+    for (let i = 0; i < items.length; i++) {
+        items[i].classList.contains('d-none') ? items[i].classList.remove('d-none') : items[i].classList.add('d-none');
+    }
+    
+
+    // Get a collection of elements with class 'sidenav-mini-icon'
+    const miniIcons = document.getElementsByClassName('sidenav-mini-icon');
+
+    // Loop through the collection and show each element
+    for (let i = 0; i < miniIcons.length; i++) {
+        miniIcons[i].classList.remove('d-none');
+    }
+    // for (let i = 0; i < miniIcons.length; i++) {
+    //     miniIcons[i].classList.contains('d-none') ? miniIcons[i].classList.remove('d-none'):miniIcons[i].classList.add('d-none');
+    // }
+
+    // document.getElementById('sidepannel').style.width="4rem";
+    const sidepanel = document.getElementById('sidepannel');
+    sidepanel.style.width = sidepanel.style.width === "14rem" ? "4rem" : "14rem";
+    // sidepanel.classList.toggle('p-2');
+
+    document.getElementById('sidepannel').className="p-2";
+    const sideBarBtn = document.querySelector('#sideBarBtn');
+    sideBarBtn.style.transform = sideBarBtn.style.transform === "translateX(8px)" ? "translateX(50px)" : "translateX(8px)";
+    
+    document.querySelector('.arrowbtn').classList.toggle('fa-arrow-left');
+    document.querySelector('.arrowbtn').classList.toggle('fa-arrow-right');
+
+    
+
+});
+
